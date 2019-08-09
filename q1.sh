8976aaf44ecff599acae22c13a9c7810c4984261
rudhi <rudhiranjan.sahoo@niser.ac.in>
2019-08-09 16:51:51 +0530

    done

---
+++ q1.sh
@@ -0,0 +1,7 @@
+#asks user for its name.
+echo what is your name?
+read Name
+mkdir $Name
+cd $Name
+touch $Name.cpp
+
---
+++ q2.sh
@@ -0,0 +1,10 @@
+#creating 5 files of user's name
+echo What is your name?
+read Name
+mkdir $Name
+cd $Name
+touch "$Name"1.cpp
+touch "$Name"2.cpp
+touch "$Name"3.cpp
+touch "$Name"4.cpp
+touch "$Name"5.cpp
